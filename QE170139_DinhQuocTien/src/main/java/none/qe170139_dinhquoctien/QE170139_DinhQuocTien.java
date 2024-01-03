72097784e5abb2201d08cf5b78a151c03516ed9c
naca09 <86470016+naca09@users.noreply.github.com>
2024-01-03 11:19:12 +0700

    mssv_fullname

---
+++ QE170139_DinhQuocTien/src/main/java/none/qe170139_dinhquoctien/QE170139_DinhQuocTien.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package none.qe170139_dinhquoctien;
+
+/**
+ *
+ * @author ASUS
+ */
+public class QE170139_DinhQuocTien {
+
+    public static void main(String[] args) {
+        System.out.println("QE170139_DinhQuocTien");
+    }
+}
