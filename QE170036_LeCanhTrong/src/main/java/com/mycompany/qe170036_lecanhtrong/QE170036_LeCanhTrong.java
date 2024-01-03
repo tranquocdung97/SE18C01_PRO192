95e2fd98730356c6ecb743f1549907209150d18e
lctrong <121157231+lctrong@users.noreply.github.com>
2024-01-03 21:32:31 +0700

    QE170036-LeCanhTrong

---
+++ QE170036_LeCanhTrong/src/main/java/com/mycompany/qe170036_lecanhtrong/QE170036_LeCanhTrong.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qe170036_lecanhtrong;
+
+public class QE170036_LeCanhTrong {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
