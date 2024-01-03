c64361a882b0aaa5c628156a871cd192317db4b1
HO VAN PHONG <giaunguyen.231074@gmail.com>
2024-01-03 09:34:59 +0700

    first commit of HO VAN PHONG

---
+++ QE180015-HOVANPHONG/src/qe180015/hovanphong/QE180015HOVANPHONG.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package qe180015.hovanphong;
+
+/**
+ *
+ * @author PC
+ */
+public class QE180015HOVANPHONG {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
