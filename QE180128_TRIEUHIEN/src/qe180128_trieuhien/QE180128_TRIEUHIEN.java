079a3fb2a351ecdfc5ead401ec23ed79249602dc
TRIEUHIEN <hientttqe180128@fpt.edu.vn>
2024-01-03 09:12:05 +0700

    first commit of TRIEUHIEN

--- QE180128_TRIEUHIEN/src/qe180128_trieuhien/QE180128_TRIEUHIEN.java
+++ QE180128_TRIEUHIEN/src/qe180128_trieuhien/QE180128_TRIEUHIEN.java
@@ -2,20 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package qe180112_le.doan.hieu;
+package qe180128_trieuhien;
 
 /**
  *
- * @author doanh
+ * @author TRIEU HIEN
  */
-public class QE180112_LEDOANHIEU {
+public class QE180128_TRIEUHIEN {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("qe180112_le.doan.hieu.QE180112_LEDOANHIEU.main()");
     }
     
 }
