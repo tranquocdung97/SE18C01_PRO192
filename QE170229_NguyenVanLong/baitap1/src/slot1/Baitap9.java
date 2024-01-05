22f846420751e91edd7efc6e0cec877cf6add0e5
Your Name <you@example.com>
2024-01-05 11:16:36 +0700

    baitap of Long

---
+++ QE170229_NguyenVanLong/baitap1/src/slot1/Baitap9.java
@@ -0,0 +1,9 @@
+package slot1;
+
+public class Baitap9 {
+    public static void main(String[] args) {
+	int a = 254;
+        int b = 343;
+	System.out.println("a + b = " + (a + b));
+	}
+}
---
+++ QE170229_NguyenVanLong/baitap1/src/slot1/baitap18.java
@@ -0,0 +1,9 @@
+package slot1;
+
+public class baitap18 {
+    public static void main(String[] args) {
+	long a = 384847522;
+        long b = 988347273;
+        System.out.println(a * b);
+    }
+}
