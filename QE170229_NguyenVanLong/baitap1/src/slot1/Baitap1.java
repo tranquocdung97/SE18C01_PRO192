2a08aa121776b173747a468885094da69d0766a9
Your Name <you@example.com>
2024-01-05 10:34:28 +0700

    baitap of Long

---
+++ QE170229_NguyenVanLong/baitap1/src/slot1/Baitap1.java
@@ -0,0 +1,7 @@
+package slot1;
+
+public class Baitap1 {
+	public static void main(String[]args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ QE170229_NguyenVanLong/baitap1/src/slot1/Baitap6.java
@@ -0,0 +1,12 @@
+package slot1;
+/*
+A simple Java program to display "Hello, World!" on the screen
+using System.out.println() statement
+*/
+public class Baitap6 {
+     public static void main(String[] args) 
+   {
+	   // Display Hello World on the screen
+	   System.out.println("Hello, World!");
+   }
+}
