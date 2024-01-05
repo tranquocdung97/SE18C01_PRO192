4b198a9cb7dec044e9f3768cfb852e37f11fd210
le quoc trieu <trieuquoc16052004@gmail.com>
2024-01-05 11:21:58 +0700

    bai1-8

---
+++ QE180029_LEQUOCTRIEU/src/qe180029_lequoctrieu/bai_7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180029_lequoctrieu;
+
+/**
+ *
+ * @author trieu
+ */
+public class bai_7 {
+    public static void main(String[] args) 
+   {
+       System.out.println("125 + 206 = "+331);
+   }
+}
