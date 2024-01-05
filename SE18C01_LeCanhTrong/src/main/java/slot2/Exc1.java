b923dec4751e44591e74fa8f5baf8c5bc6a95501
lctrong <121157231+lctrong@users.noreply.github.com>
2024-01-05 11:41:54 +0700

    first program and comments_LeCanhTrong

---
+++ SE18C01_LeCanhTrong/src/main/java/slot2/Exc1.java
@@ -0,0 +1,52 @@
+
+package slot2;
+/*
+Task
+Fill in the blank (...) to complete to the program that displays the following line on the screen:
+
+Hello, World!
+Theory
+In Java, to print a line of text on the screen, we use System.out.println() stetament. For example, to display the text "Hello Java" on the screen, do the following:
+
+public class HelloWorld {
+	public static void main(String[]args) {
+		System.out.println("Hello Java");
+	}
+}
+When the above code is compiled and executed, it produces the following result:
+
+Hello Java
+There are some statements in the code that you are not familiar with such as public class HelloWorld or public static void main(String[]args) but you do not have to pay attention to them in this lesson. You can assume that they are the required parts of a Java program.
+
+Now you can write a program to display your name on the screen. For example, if your name is Nguyen Van Nam, the program that displays your name is as below:
+
+public class HelloWorld {
+	public static void main(String[]args) {
+		System.out.println("Nguyen Van Nam");
+	}
+}
+In a Java program, System.out.println() is used to print values on the screen.
+
+Now you can return to the Task and solve it or refer to the instruction below.
+
+Instruction
+To solve the task, you just need to replace ... with System.out.println("Hello, World!");
+
+Code sample:
+
+public class HelloWorld {
+	public static void main(String[]args) {
+		System.out.println("Hello, World!");
+	}
+}
+Read more: 
+
+https://codelearn.io/sharing/top-10-ide-java-2020 
+
+https://codelearn.io/sharing/tong-hop-50-keyword-can-nho-trong-java 
+*/
+public class Exc1 {
+    	public static void main(String[]args) {
+		System.out.println("Hello, World!");
+	}
+}
---
+++ SE18C01_LeCanhTrong/src/main/java/slot2/Exc4.java
@@ -0,0 +1,61 @@
+/*
+Basic Java Arithmetic operations
+ */
+package slot2;
+
+/**
+Task
+Write a program to calculate the sum of 313 and 122.
+
+Theory
+In addition to using System.out.println() statement to print information of characters on the screen, you can use this statement to print the sum, difference, product or quotient. See the example below:
+
+public class HelloWorld
+{
+   public static void main(String[] args) 
+   {
+       System.out.println(8 + 4);
+       System.out.println(8 - 4);
+       System.out.println(8 * 4);
+       System.out.println(8 / 4);
+   }
+}
+When the above code is compiled and executed, it produces the following result:
+
+12
+4
+32
+2
+Now you can write a simple Java program to calculate the sum, difference, product or quotient.
+
+An example that calculates the product of 17 and 18:
+
+public class HelloWorld
+{
+   public static void main(String[] args) 
+   {
+       System.out.println(17 * 18);
+   }
+}
+When the above code is compiled and executed, it produces the following result:
+
+306
+Now you can return to the Task and solve it or refer to the instruction below.
+
+Instruction
+Code sample:
+
+public class HelloWorld
+{
+   public static void main(String[] args) 
+   {
+       System.out.println(313 + 122);
+   }
+}
+ */
+public class Exc4 {
+       public static void main(String[] args) 
+   {
+       System.out.println(313 + 122);
+   }
+}
