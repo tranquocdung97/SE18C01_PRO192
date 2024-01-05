7cbcc539ae9ce7b5171e08c451ab21d303b99b01
unknown <QE180033_nguyet>
2024-01-05 14:55:55 +0700

    bai 19

---
+++ QE180033_NguyenNuNhuNguyet/src/qe180033_nguyennunhunguyet/bai19.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180033_nguyennunhunguyet;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class bai19 {
+    public static void main(String[] args) {
+        boolean b=true;
+        System.out.println(b);
+    }
+}
