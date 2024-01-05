5ebde6991b10fcbdb5bd4f2f95eab4571172548f
nguyetQe180033 <155499573+nguyetQe180033@users.noreply.github.com>
2024-01-05 11:00:19 +0700

    Update Baitap1.java

--- baitap1/src/baitap1/Baitap1.java
+++ baitap1/src/baitap1/Baitap1.java
@@ -10,11 +10,9 @@ package baitap1;
  */
 public class Baitap1 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello, World!");
     }
     
 }
