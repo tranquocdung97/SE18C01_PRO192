ad5ee6de6a2bbba419e34129ff79ce2e5b7c351e
Bui Xuan Hien <buixuanhien2004qn@gmail.com>
2024-01-06 19:31:22 +0700

    bai tap xuan hien

---
+++ QE180008_BuiXuanHien/src/bai15.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author XUAN HIEN
+ */
+public class bai15 {
+    public static void main(String[] args) { 
+        double chieudai=7.5, chieurong=3.8;  
+        System.out.println("Area = " + chieudai*chieurong);
+    }
+}
