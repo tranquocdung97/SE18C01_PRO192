577d178890f9a27c161fce9442b76bce0d187f0d
Your Name <you@example.com>
2024-01-08 22:15:38 +0700

    bai tap of Long

--- QE170229_NguyenVanLong/baitap1/src/slot1/baitap19.java
+++
@@ -1,8 +0,0 @@
-package slot1;
-
-public class baitap19 {
-    public static void main(String[] args) {
-	boolean b = true;
-	System.out.println(b);
-    }
-}
--- QE170229_NguyenVanLong/src/qe170229_nguyenvanlong/QE170229_NguyenVanLong.java
+++
@@ -1,9 +0,0 @@
-package qe170229_nguyenvanlong;
-
-public class QE170229_NguyenVanLong {
-
-    public static void main(String[] args) {
-      System.out.println("qe170229_NguyenVanLong");
-    }
-    
-}
