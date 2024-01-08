c4e3e8f23ee01aa840f8f1e6b41d16db86113c11
Your Name <you@example.com>
2024-01-08 22:36:54 +0700

    bai tap cua Long

--- QE170229_NguyenVanLong/First program and comments/src/Bt8.java
+++ QE170229_NguyenVanLong/First program and comments/src/Bt8.java
@@ -1,6 +1,6 @@
 public class Bt8 {
 	public static void main(String[] args) {
-		System.out.println("2468 + 1234 = " + (2468 + 1234));
+	System.out.println("2468 + 1234 = " + (2468 + 1234));
         System.out.println("2468 - 1234 = " + (2468 - 1234));
         System.out.println("2468 * 1234 = " + (2468 * 1234));
         System.out.println("2468 / 1234 = " + (2468 / 1234));
