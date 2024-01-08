c19a35c8daf3520325a877c3168dc60bd3465535
NguyenVanDiep <DiepNVQE180203@fpt.edu.vn>
2024-01-08 10:34:45 +0700

    Exercise of VanDiep

---
+++ QE180203_NguyenVanDiep/src/qe180203_nguyenvandiep/Slot1_Exercise16.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qe180203_nguyenvandiep;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Slot1_Exercise16 {
+    public static void main(String[] args) {
+		char c = 'x';
+		System.out.println(c);
+    }
+}
