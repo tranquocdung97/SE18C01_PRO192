24cf1dc2ddbb5d7c5e939623fc93400d56681e96
Riema <155499415+Hunteriema@users.noreply.github.com>
2024-01-09 17:51:08 +0700

    Add files via upload

---
+++ QE180149_DuongThao/src/QE180149_DuongThao/BT11.java
@@ -0,0 +1,13 @@
+package QE180149_DuongThao;
+
+public class BT11 {
+
+    public static void main(String args[]) {
+        int a = 6;
+        int b = 2;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
---
+++ QE180149_DuongThao/src/QE180149_DuongThao/BT15.java
@@ -0,0 +1,10 @@
+package QE180149_DuongThao;
+
+public class BT15 {
+
+    public static void main(String args[]) {
+        double a = 27.5;
+        double b = 1;
+        System.out.println("Area = " + (a + b));
+    }
+}
