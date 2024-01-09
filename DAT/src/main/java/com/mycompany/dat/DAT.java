21f571f7cdc006621db1233997558d1fa0d4b596
LE DOAN HIEU <ledoanhieu12a6@gmail.com>
2024-01-09 09:47:37 +0700

    NGUYEN QUOC DAT

---
+++ DAT/src/main/java/com/mycompany/dat/DAT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dat;
+
+/**
+ *
+ * @author doanh
+ */
+public class DAT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
