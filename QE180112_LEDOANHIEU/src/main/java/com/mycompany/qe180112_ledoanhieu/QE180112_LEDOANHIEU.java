47748fe120e2e83f01530021e03c19a3022b5d57
LE DOAN HIEU <ledoanhieu12a6@gmail.com>
2024-01-09 23:10:38 +0700

    Exercise

---
+++ QE180112_LEDOANHIEU/src/main/java/com/mycompany/qe180112_ledoanhieu/QE180112_LEDOANHIEU.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qe180112_ledoanhieu;
+
+/**
+ *
+ * @author doanh
+ */
+public class QE180112_LEDOANHIEU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
