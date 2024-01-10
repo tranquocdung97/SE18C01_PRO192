33ac036a7a1cc08b9c229e2d8fe365524e88ac63
Your Name <you@example.com>
2024-01-10 08:34:11 +0700

    bai tap cua Long

--- QE170229_NguyenVanLong/Operators/src/Bt1.java
+++ QE170229_NguyenVanLong/Operators/src/Bt1.java
@@ -1,8 +1,10 @@
+
 import java.util.Scanner;
 
 public class Bt1 {
+
     public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello " + sc.next());
     }
 }
