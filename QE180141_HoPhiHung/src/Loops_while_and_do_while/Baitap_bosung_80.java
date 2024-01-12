c539c1ab585ffd74e64ea70a9db256a217f27698
Ho Phi Hung <hophihunghh123@gmail.com>
2024-01-12 09:58:11 +0700

    ho phi hung bo sung

---
+++ QE180141_HoPhiHung/src/Loops_while_and_do_while/Baitap_bosung_80.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Loops_while_and_do_while;
+
+/**
+ *
+ * @author Ho Phi Hung
+ */
+public class Baitap_bosung_80 {
+ //public class Loop {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 100; i++) {
+			if ( i < 0 || i >  50  ){ break ;}
+			System.out.print(i + " ");
+		}
+	}
+}
