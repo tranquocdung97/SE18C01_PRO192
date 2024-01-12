24444dc3994c841a7baeab9ad4651defaec293bb
Ho Phi Hung <hophihunghh123@gmail.com>
2024-01-12 09:06:58 +0700

    ho phi hung

--- QE180141_HoPhiHung/src/Variables_and_Types/Baitap_9.java
+++ QE180141_HoPhiHung/src/Variables_and_Types/Baitap_9.java
@@ -9,5 +9,8 @@ package Variables_and_Types;
  * @author Ho Phi Hung
  */
 public class Baitap_9 {
-    
+    public static void main(String[] args) {
+		int a= 254, b= 343;
+		System.out.println("a + b = " + (a + b));
+	}
 }
