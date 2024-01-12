f6e6e4c6b0a1b78efdfccc40051d009ce853b42e
Thanh Long <ntl07092004@gmail.com>
2024-01-12 11:12:19 +0700

    bt of long

---
+++ QE180148_NGUYEN THANH LONG/EX/Exercise Slot 1/Btvn34.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class Btvn34 {
+public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        if (a.equals(b))
+        {
+            System.out.println("two people have the same name");
+        }
+        else 
+        {
+            System.out.println("two people don't have the same name");
+        }
+	}    
+}
