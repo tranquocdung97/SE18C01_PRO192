aa48a04672dc269dae9f7e137dd2bd239a1bf016
le quoc trieu <trieuquoc16052004@gmail.com>
2024-01-15 21:08:35 +0700

    bai tap

---
+++ src/qe180029_lequoctrieu/Loopstatements/bai1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180029_lequoctrieu.Loopstatements;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author trieu
+ */
+public class bai1 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+		for (int i = 1; i <= n; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
