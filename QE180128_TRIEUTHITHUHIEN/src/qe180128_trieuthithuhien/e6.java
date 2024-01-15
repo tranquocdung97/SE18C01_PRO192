1ec350f44aaf5fc9d56a5eaed7ccdc89f5db68de
TRIEUHIEN <hientttqe180128@fpt.edu.vn>
2024-01-15 22:32:10 +0700

    bai tap Strings

---
+++ QE180128_TRIEUTHITHUHIEN/src/qe180128_trieuthithuhien/e6.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180128_trieuthithuhien;
+
+/**
+ *
+ * @author TRIEU HIEN
+ */
+public class e6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        for(char c = '0'; c <= '9'; c++){
+            s = s.replace(c + "", "");
+        }
+        System.out.print(s);
+    }
+}
