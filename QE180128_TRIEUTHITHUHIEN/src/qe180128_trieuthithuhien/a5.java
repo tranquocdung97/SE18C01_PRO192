27780c7b123f394ea18edd2a9ee7bcb11f2114fd
TRIEUHIEN <hientttqe180128@fpt.edu.vn>
2024-01-15 21:19:22 +0700

    bai tap Loop statements

---
+++ QE180128_TRIEUTHITHUHIEN/src/qe180128_trieuthithuhien/a5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180128_trieuthithuhien;
+
+/**
+ *
+ * @author TRIEU HIEN
+ */
+public class a5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int answer = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                answer += i;
+            }
+        }
+        System.out.println(answer);
+    }
+}
