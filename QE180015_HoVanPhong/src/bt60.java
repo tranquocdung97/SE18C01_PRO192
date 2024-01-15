b8d28acb684fb7f05808bce4b421e9f2073241e2
HO VAN PHONG <giaunguyen.231074@gmail.com>
2024-01-15 08:53:51 +0700

    arrays

---
+++ QE180015_HoVanPhong/src/bt60.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author PC
+ */
+import java.util.Scanner;
+public class bt60 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        int [] a = new int [n];
+        for(int i = 0; i< n;i++){
+            a[i] = sc.nextInt();
+        }
+        System.out.print(a[0] + a[n-1]);
+	}
+}
