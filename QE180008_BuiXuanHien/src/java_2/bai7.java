3fc1f54c3dc2da1f5c6a50140ad6af50872e4b03
Bui Xuan Hien <buixuanhien2004qn@gmail.com>
2024-01-17 09:12:35 +0700

    bai tap java 2

---
+++ QE180008_BuiXuanHien/src/java_2/bai7.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java_2;
+
+/**
+ *
+ * @author XUAN HIEN
+ */
+class Student {
+
+    String name;
+    int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
+public class bai7 {
+
+    public static void main(String[] args) {
+        Student[] students = new Student[3];
+        students[0] = new Student("Tuan", 24);
+        students[1] = new Student("Cuong", 25);
+        students[2] = new Student("Duc", 24);
+        for (int i = 0; i < 3; i++) {
+            students[i].display();
+        }
+    }
+}
