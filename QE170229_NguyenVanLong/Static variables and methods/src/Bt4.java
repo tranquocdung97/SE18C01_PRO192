c6feb8653b6cd23a433afaef0b7417871bf2441d
longbgo <longnvqe170229@fpt.edu.vn>
2024-01-19 10:02:56 +0700

    bai tap cua Long

---
+++ QE170229_NguyenVanLong/Static variables and methods/src/Bt4.java
@@ -0,0 +1,16 @@
+public class Bt4 {
+    private String name;
+    private int age;
+
+    public Student() {
+
+    }
+
+    public void getInformation() {
+
+    }
+
+    public void display() {
+        
+    }
+}
