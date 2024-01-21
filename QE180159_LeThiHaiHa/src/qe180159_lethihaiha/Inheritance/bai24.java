94f42fc976ff891c88c80bbfabd6224093f17efa
QE180159_LeTHiHaiHa <halthqe180159@fpt.edu.vn>
2024-01-21 16:50:49 +0700

    haiha

---
+++ QE180159_LeThiHaiHa/src/qe180159_lethihaiha/Inheritance/bai24.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180159_lethihaiha.Inheritance;
+
+/**
+ *
+ * @author admin
+ */
+public class bai24 {
+     private String name;
+    private int age;
+
+    public bai24(){
+
+    }
+    public void getInformation(){
+
+    }
+    public void display(){
+
+    }
+}
