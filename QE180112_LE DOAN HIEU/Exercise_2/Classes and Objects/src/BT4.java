1430449575ec60885e4f623828726fc9363c2d45
LE DOAN HIEU <152629519+OTISDOAN@users.noreply.github.com>
2024-01-21 21:21:18 +0700

    Completed deadline 2

---
+++ QE180112_LE DOAN HIEU/Exercise_2/Classes and Objects/src/BT4.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author doanh
+ */
+class Student {
+
+    private String name;
+    private int age;
+
+    public Student() {
+
+    }
+
+    public void getInformation() {
+
+    }
+
+    public void display() {
+
+    }
+}
+
+public class BT4 {
+    public static void main(String[] args) {
+        
+    }
+}
