ea90f0ee99dc64cca0c1a7cb43e1789024f37c48
Nguyen Quoc Dat <datnqqe180097@fpt.edu.vn>
2024-01-22 16:21:07 +0700

    bai_tap_4

---
+++ QE180097-NGUYEN QUOC DAT/bt4/src/bt6.java
@@ -0,0 +1,23 @@
+class Circle{
+    private double radius;
+
+    public  Circle(double radius){
+            this.radius = radius;
+    }
+
+    public double getArea(){
+        return 3.14 * radius * radius;
+    }
+
+    public double getCircumference(){
+        return 3.14 * 2 * radius;
+    }
+}
+public class bt6 {
+    	public static void main(String[] args) {
+            Circle c = new Circle(7);
+            System.out.println(c.getArea());
+            System.out.println(c.getCircumference());
+
+        }
+}
