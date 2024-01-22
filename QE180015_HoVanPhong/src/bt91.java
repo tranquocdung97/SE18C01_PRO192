e0e7fb23c1bdf4a59559c0a4a5748c16afbcb1b3
HO VAN PHONG <giaunguyen.231074@gmail.com>
2024-01-22 08:25:55 +0700

    Encapsulation

--- QE180015_HoVanPhong/src/bt91.java
+++ QE180015_HoVanPhong/src/bt91.java
@@ -7,6 +7,23 @@
  *
  * @author PC
  */
+public class ArrayCalculator {
+	public static int sumOfArray(int arr[]) {
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		return sum;
+	}
+
+	public static double sumOfArray(double arr[]) {
+		double sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		return sum;
+	}
+}
 public class bt91 {
     
 }
--- QE180015_HoVanPhong/src/bt92.java
+++ QE180015_HoVanPhong/src/bt92.java
@@ -7,6 +7,44 @@
  *
  * @author PC
  */
+public class ArrayCalculator{
+    public static int maxOfArray(int arr[]){
+        int max = arr[0];
+        for(int i = 1; i< arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static double maxOfArray(double arr[]){
+        double max = arr[0];
+        for(int i = 1; i< arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public static int minOfArray(int arr[]){
+        int min = arr[0];
+        for(int i = 1; i< arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+    public static double minOfArray(double arr[]){
+        double min = arr[0];
+        for(int i = 1; i< arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
 public class bt92 {
     
 }
