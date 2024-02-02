db89fb3734fd5a0acab6957e40bc6e344c457a06
nguyetQe180033 <nguyetnnnqe180033@fpt.edu.vn>
2024-02-02 09:50:00 +0700

    Nhu Nguyet

---
+++ QE180033_NguyenNuNhuNguyet/JavaApplication4/src/qe180033_nguyennunhunguyet/bai1.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180033_nguyennunhunguyet;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class bai1 {
+    public static void main(String[]args) {
+	System.out.println("Hello, World!");
+    }
+}
---
+++ QE180033_NguyenNuNhuNguyet/JavaApplication4/src/qe180033_nguyennunhunguyet/bai57.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180033_nguyennunhunguyet;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class bai57 {
+    public static void main(String[] args) {
+        int i = 1;
+        do 
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+        while (i<=5);
+    }
+}
