41eb68a5c45d93ede85a448fd060c1c4ab888227
Thanh Long <ntl07092004@gmail.com>
2024-02-20 21:05:24 +0700

    btvn of long

---
+++ QE180148_NGUYEN THANH LONG/EX/BT tet/ClockAngleCalculator.java
@@ -0,0 +1,85 @@
+
+import java.util.Date;
+import java.util.Scanner;
+
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * 
+ */
+
+
+public class ClockAngleCalculator {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the hour (1-12): ");
+        int hour = scanner.nextInt();
+
+        System.out.print("Enter the minute (0-59): ");
+        int minute = scanner.nextInt();
+
+        
+        double angle = calculateClockAngle(hour, minute);
+        System.out.println("The angle between the hour and minute hands is: " + formatAngle(angle));
+    }
+
+    
+    private static double calculateClockAngle(double hour, double minute) {
+        
+        hour = hour % 12;
+        
+
+        
+        double angle = Math.abs(30 * hour - 11.0 / 2 * minute + 1.0 / 2 * minute);
+
+        
+        angle = Math.min(angle, 360 - angle);
+
+        
+        return Math.toRadians(angle);
+    }
+
+    
+    private static String formatAngle(double angle) {
+        
+        int numerator = (int) (angle * 180 / Math.PI);
+        int denominator = 180;
+
+        
+        int gcd = findGCD(numerator, denominator);
+
+        
+        numerator /= gcd;
+        denominator /= gcd;
+
+        
+        if (denominator == 1) {
+            return Integer.toString(numerator);
+        } else {
+            
+            return numerator + "pi/" + denominator;
+        }
+    }
+
+    
+    private static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
---
+++ QE180148_NGUYEN THANH LONG/EX/BT tet/bai2.java
@@ -0,0 +1,57 @@
+
+import static java.lang.Math.pow;
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * 
+ */
+public class bai2 {
+    
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhap vao day so (1-10^11)");
+        long a = sc.nextLong();
+        if(dem(a) == 0 ){ 
+        System.out.println("nhap vao sai");
+        }else{
+        System.out.println(" "+checksumPart1(a));
+       }
+    }
+    private static int checksumPart1(long a){
+        int sum =0;
+        int n = dem(a);
+        for(int i=1;i<=n+1;i++){
+            if(i%2 == 0){
+                sum += (a%10 *1);
+            }else{
+                sum += (a%10 *3);
+            }
+            a/=10;
+        }
+        sum=checksum(sum);
+        return sum;
+    }
+    
+    private static int checksum(int sum){
+        int tong = (sum/10+1)*10; 
+        sum = Math.abs(tong - sum);
+        return sum;
+    }
+    
+    private static int dem(long a){
+        String str = String.valueOf(a);
+        int n = str.length();
+        if(n<0 && n> 11){
+            return 0;
+        }
+        return n;
+    }
+}
