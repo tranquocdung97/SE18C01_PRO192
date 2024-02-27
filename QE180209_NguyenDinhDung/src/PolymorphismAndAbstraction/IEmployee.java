eccef92b64043491df832eadebb0e4e4f33d14e8
Your Name <you@example.com>
2024-02-27 21:32:31 +0700

    file cua dung

---
+++ QE180209_NguyenDinhDung/src/PolymorphismAndAbstraction/IEmployee.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PolymorphismAndAbstraction;
+
+/**
+ *
+ * @author Administrator
+ */
+public interface IEmployee {
+	int calculateSalary();
+	String getName();
+}
