ebd59dc42e47600c256793e74bcdb033a4d9d334
Riema <155499415+Hunteriema@users.noreply.github.com>
2024-02-28 07:23:35 +0700

    Added Remaining

---
+++ QE180149_DuongThao/src/QE180149_DuongThao_OOP/BT28/Employee.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package QE180149_DuongThao.OOP.BT28;
+
+public abstract class Employee implements IEmployee {
+
+    private String name;
+    private int paymentPerHour;
+
+    public Employee(String name, int paymentPerHour) {
+        this.name = name;
+        this.paymentPerHour = paymentPerHour;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPaymentPerHour() {
+        return paymentPerHour;
+    }
+
+    public void setPaymentPerHour(int paymentPerHour) {
+        this.paymentPerHour = paymentPerHour;
+    }
+}
