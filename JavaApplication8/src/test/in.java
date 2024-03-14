793f969d76c5cd8ea67c09c51c9cae24c401e192
HO VAN PHONG <giaunguyen.231074@gmail.com>
2024-03-14 11:31:16 +0700

    baitapandproject

---
+++ JavaApplication8/src/test/in.java
@@ -0,0 +1,11 @@
+
+package test;
+
+import Controller.giangVienController;
+
+public class in {
+    public static void main(String[] args) {
+        giangVienController ee = new giangVienController();
+        ee.menu();
+    }
+}
