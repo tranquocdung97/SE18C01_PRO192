63c59b582a9b99a0163747274c9429f3a5b9fd2b
vinhtdqqe180174 <146817300+vinhtdqqe180174@users.noreply.github.com>
2024-03-22 10:31:59 +0700

    Segment

---
+++ QE180174_ThanDucQuangVinh/src/qe180174_thanducquangvinh/bai25.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180174_thanducquangvinh;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author HOANG KHUONG
+ */
+public class bai25 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("a % b = " + (a - b * (a / b)));
+    }
+}
---
+++ QE180174_ThanDucQuangVinh/src/qe180174_thanducquangvinh/bai5.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qe180174_thanducquangvinh;
+
+/**
+ *
+ * @author HOANG KHUONG
+ */
+public class bai5 {
+    public static void main(String[] args) {
+         System.out.println(37 * 56);
+    }
+}
